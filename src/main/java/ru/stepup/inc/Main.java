c6dd794350ebde1e1512e37f5b99544f747500c6
Pavel <skhnvp@gmail.com>
2025-03-16 17:34:01 +0300

    init

--- src/main/java/ru/stepup/inc/Main.java
+++ src/main/java/ru/stepup/inc/Main.java
@@ -23,7 +23,7 @@ package ru.stepup.inc;
 public class Main {
     public static void main(String[] args) {
         Fraction f1 = new Fraction(1, 3);
-        Fraction f2 = new Fraction(-2, 5);
+        Fraction f2 = new Fraction(2, 5);
         Fraction f3 = new Fraction(7, 8);
 
         System.out.println("Результат: " + f1.sum(f2).sum(f3).minus(5));
